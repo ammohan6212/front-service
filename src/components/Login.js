c3d5fe0630225c5952339b5e88759f305e62fd2d
A.Mohan <mohan6121@email.com>
2025-05-17 15:03:33 +0530

    mohan

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("/login", {
+    const response = await fetch("/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
