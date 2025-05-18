6e80e42454d49a5828aaaaff1d28d63f32acc894
A.Mohan <mohan6121@email.com>
2025-05-18 09:10:01 +0530

    bacekend

--- src/components/Register.js
+++ src/components/Register.js
@@ -26,7 +26,7 @@ function Register() {
     }
   };
 
-  const handleClear = () => {zz
+  const handleClear = () => {
     setForm({ username: "", email: "", password: "" });
     setMessage("");
   };
