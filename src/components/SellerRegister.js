d42a6e061896c201377d91786a021ec89c53fac1
A.Mohan <mohan6121@email.com>
2025-06-07 14:25:24 +0530

    mohan

--- src/components/SellerRegister.js
+++ src/components/SellerRegister.js
@@ -47,10 +47,14 @@ function SellerRegister() {
 
     try {
       const response = await fetch("/api/start-registration", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: form.email }),
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+              username: form.username,
+              email: form.email
+          }),
       });
+
 
       const data = await response.json();
 
